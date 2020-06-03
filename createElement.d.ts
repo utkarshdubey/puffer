d687ea067e8d31b1543ced8c48b1a6ec14fffa1a
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-06-03 22:10:46 +0530

    Initial commit

---
+++ createElement.d.ts
@@ -0,0 +1,4 @@
+export declare function createElement(type: any, props: any, ...children: any[]): {
+    type: any;
+    props: any;
+};
---
+++ render.d.ts
@@ -0,0 +1,1 @@
+export declare function render(element: any, container: any): void;
